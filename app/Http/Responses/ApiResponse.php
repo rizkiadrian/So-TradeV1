3944edbc54fe22e6dbea6099144555316409c6fb
Rizki Adrian <rizkiadrian39@gmail.com>
2024-10-03 00:37:23 +0700

    feat(docs): Add docks for response helper

--- app/Http/Responses/ApiResponse.php
+++ app/Http/Responses/ApiResponse.php
@@ -5,6 +5,14 @@ namespace App\Http\Responses;
 use Illuminate\Http\JsonResponse;
 
 class ApiResponse {
+    /**
+     * Returns a successful response with a message
+     *
+     * @param null $data
+     * @param string $message
+     * @param int $statusCode
+     * @return JsonResponse
+     */
     public static function success($data = null, $message = 'Data fetched successfully', $statusCode = 200) {
         $apiResponse = [
             "meta" => [
@@ -19,6 +27,15 @@ class ApiResponse {
         return response()->json($apiResponse, $statusCode);
     }
 
+
+    /**
+     * Returns a failed response with a message
+     *
+     * @param null $error
+     * @param string $message
+     * @param int $statusCode
+     * @return JsonResponse
+     */
     public static function fail($error = null, $message = 'An Error occured', $statusCode = 400) {
         $apiResponse = [
             "meta" => [
