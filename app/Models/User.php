204af4488dc1ca9407a861897cdba4f94422efa3
Rizki Adrian <rizkiadrian39@gmail.com>
2024-10-10 13:28:26 +0700

    refactor(typo): Wrong model name in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,7 +73,7 @@ class User extends Authenticatable
      */
     public function finances()
     {
-        return $this->hasOne(UserFinance::class);
+        return $this->hasOne(UserFinancial::class);
     }
 
     /**
